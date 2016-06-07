9a416b615c6aebd13f3fbb7f0c6b0dcd9065fa07
Arthur-Hinsvark <ajhinsvark@gmail.com>
2016-06-07 18:06:35 -0400

    Added Deck Class
    Able to keep track of cards the player has in deck and preform necessary
    functions such as shuffling the deck, drawing a card, and peeking at a
    card.

---
+++ Assets/Scripts/Deck.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Deck : MonoBehaviour {
+    //The deck class will hold the list of cards the user has in their deck.
+    //Variables
+    
+    //List of the cards in deck - index 0 is the bottom while cards.Count is the top card
+    List<Card> cards = new List<Card>();
+
+    //Empty constructor
+    public Deck()
+    {
+
+    }
+
+    /// <summary>
+    /// Shuffle the deck
+    /// </summary>
+    public void Shuffle()
+    {
+        int n = cards.Count;
+        while(n > 1)
+        {
+            n--;
+            int k = Random.Range(0, n+1);
+            Card c = cards[k];
+            cards[k] = cards[n];
+            cards[n] = c;
+        }
+    }
+
+    /// <summary>
+    /// Draw the single topmost card
+    /// </summary>
+    public Card Draw()
+    {
+        Card drawn = cards[cards.Count];
+
+        //Remove card from deck
+        cards.RemoveAt(cards.Count);
+        return drawn;
+    }
+
+    /// <summary>
+    /// Draw N cards returning a List with the top card at index 0
+    /// </summary>
+    public List<Card> Draw(int n)
+    {
+        List<Card> drawn = new List<Card>();
+        for(int i = 0; i < n; i++)
+        {
+            drawn[i] = Draw();
+        }
+
+        return drawn;
+    }
+    
+    /// <summary>
+    /// View the top n cards with the top card at index 0
+    /// </summary>
+    public List<Card> View(int n)
+    {
+        List<Card> peek = new List<Card>();
+        for (int i = 0; i < n; i++)
+        {
+            peek[i] = View();
+        }
+
+        return peek;
+    }
+
+    /// <summary>
+    /// View the top most card
+    /// </summary>
+    public Card View()
+    {
+        return cards[cards.Count];
+    }
+
+    #region Required overides for MonoBehavior - may be removed later
+    // Use this for initialization
+    void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    #endregion
+}
