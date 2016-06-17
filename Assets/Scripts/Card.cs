b33a002ca8a8eac8bc5ea6d33dc152273f24cc4b
Arthur-Hinsvark <ajhinsvark@gmail.com>
2016-06-16 20:43:41 -0400

    3D Card prefab added!
    New drag and drop capabilities for 3D cards (glorified rectangular
    prisms)

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -2,7 +2,16 @@
 using System.Collections;
 using UnityEngine.EventSystems;
 
-public class Card : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+public class Card : MonoBehaviour {
+
+    private Vector3 screenPoint;
+    private Vector3 offset;
+    public string title;
+
+    public Card()
+    {
+        title = Random.Range(0, 10000).ToString();
+    }
 
     // Use this for initialization
     void Start() {
@@ -14,22 +23,41 @@ public class Card : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHand
 
     }
 
-    private Vector2 grabPoint;
-    public void OnBeginDrag(PointerEventData eventData)
+    //, IBeginDragHandler, IDragHandler, IEndDragHandler
+
+    //private Vector2 grabPoint;
+    //public void OnBeginDrag(PointerEventData eventData)
+    //{
+    //    //Keep the card "held" at the same position during the drag instead of snapping to the center
+    //    float diffX = this.transform.position.x - eventData.position.x;
+    //    float diffY = this.transform.position.y - eventData.position.y;
+    //    grabPoint = new Vector2(diffX, diffY);
+    //}
+
+    //
+    void OnMouseDown()
     {
-        //Keep the card "held" at the same position during the drag instead of snapping to the center
-        float diffX = this.transform.position.x - eventData.position.x;
-        float diffY = this.transform.position.y - eventData.position.y;
-        grabPoint = new Vector2(diffX, diffY);
+        screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+        Debug.Log("Title: " + title);
+        offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
     }
 
-    public void OnDrag(PointerEventData eventData)
+    void OnMouseDrag()
     {
-        this.transform.position = eventData.position + grabPoint;
+        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+
+        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+        transform.position = curPosition;
     }
 
-    public void OnEndDrag(PointerEventData eventData)
-    {
-        Debug.Log("END DRAG");
-    }
+
+    //public void OnDrag(PointerEventData eventData)
+    //{
+    //    this.transform.position = eventData.position + grabPoint;
+    //}
+
+    //public void OnEndDrag(PointerEventData eventData)
+    //{
+    //    Debug.Log("END DRAG");
+    //}
 }
